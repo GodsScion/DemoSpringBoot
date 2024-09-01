367adfac5bf1a301c0edfe9ea6a9e5d7fed2fd49
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2024-08-31 18:18:46 -0700

    Compiler injection

--- src/main/java/com/nibokka/myApp/Dev.java
+++ src/main/java/com/nibokka/myApp/Dev.java
@@ -1,14 +1,18 @@
 package com.nibokka.myApp;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class Dev {
     
-    @Autowired          // Field injection
+    // @Autowired          // Field injection
     Computer computer;
+
+    public Dev(Computer computer) { // Constructor injection, no need to use @Autowired, but you'll need to declare variable "Computer computer;" like in line 11
+        this.computer = computer;
+    }
 
     public void build() {
         computer.compile();
