ca3a8f56e7cb7caeab3cda7dbee86c88deeb2d7e
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2024-08-31 18:16:28 -0700

    Field injection

---
+++ src/main/java/com/nibokka/myApp/Computer.java
@@ -0,0 +1,5 @@
+package com.nibokka.myApp;
+
+public interface Computer {
+    public void compile();
+}
--- src/main/java/com/nibokka/myApp/Dev.java
+++ src/main/java/com/nibokka/myApp/Dev.java
@@ -1,12 +1,18 @@
 package com.nibokka.myApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class Dev {
     
+    @Autowired          // Field injection
+    Computer computer;
+
     public void build() {
-        System.out.println("Dev building stuff");
+        computer.compile();
+        System.out.println("Dev building stuff");        
     }
 
 }
