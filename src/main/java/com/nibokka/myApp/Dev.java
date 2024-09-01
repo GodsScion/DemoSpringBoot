1fb2595393c174f12008da51c46d0b94f847e36e
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2024-08-31 18:27:39 -0700

    Primary annotation is needed when two children Laptop and desktop implement Computer

--- src/main/java/com/nibokka/myApp/Dev.java
+++ src/main/java/com/nibokka/myApp/Dev.java
@@ -7,17 +7,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class Dev {
     
-    // @Autowired           // Field injection
+    @Autowired           // Field injection (Apparently not recommended!)
     Computer computer;      // You need to declare variables in all injection types (field, constructor, setter)
 
-    // public Dev(Computer computer) { // Constructor injection, no need to use @Autowired
+    // public Dev(Computer computer) { // Constructor injection, no need to use @Autowired  (Good practice to use constructor or setter injection)
     //     this.computer = computer;
     // }
 
-    @Autowired              // Setter injection
-    public void setComputer(Computer computer) {
-        this.computer = computer;
-    }
+    // @Autowired              // Setter injection
+    // public void setComputer(Computer computer) {
+    //     this.computer = computer;
+    // }
 
     public void build() {
         computer.compile();
